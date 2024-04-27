f0a4e29186c953f36171ca098ad40944fc27d13b
shashiKiran <kiranshashi110@gmail.com>
2024-04-27 18:12:46 +0530

    Create Seeder File for Academic Levels Table

---
+++ database/seeders/academicLevelSeeder.js
@@ -0,0 +1,37 @@
+// Imports from server files
+const connect = require("./../connect");
+
+// This query will flush out all the rows that are in the academic_levels Table
+const flushAcLevels = `
+    DELETE FROM academic_levels;
+`;
+
+// This query will reseed the academic_levels table with data collected on
+const seedAcLevels = `
+    INSERT INTO academic_levels (level_name) 
+    VALUES
+        ('Undergraduate'),
+        ('Postgraduate'),
+        ('Doctorate');
+`;
+
+(async function () {
+    let connection;
+
+    try {
+        connection = await connect();
+
+        await connection.query(flushAcLevels);
+        await connection.query(seedAcLevels);
+
+        console.log("PASS    DB_SD_07");
+    } catch (error) {
+
+        console.log("ERROR    DB_SD_07");
+        console.log(error);
+
+    } finally {
+        connection.end();
+    }
+
+})();
