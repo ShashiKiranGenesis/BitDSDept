0f26a0c46d62729b563bdde71f1917e430101ad8
shashiKiran <kiranshashi110@gmail.com>
2024-04-28 20:10:47 +0530

    Create mySQL transaction to get details of ISTH based on given vtu_id and year

---
+++ database/tables/i_s_t_h/getDetailsByVtuIdAndYear.js
@@ -0,0 +1,59 @@
+// Imports from server files
+const connect = require("./../../connect");
+const generateResponse = require("./../../../helpers/response/generateResponse");
+
+// The Query to fetch isthDetails of an Employee belonging to a particular year
+const getISTHDetailsByVtuIdAndYear = `
+    SELECT 
+        isth.id AS id,
+        c.name AS course_name,
+        al.name AS level,
+        c.cpw AS classes_per_week,
+        isth.tce AS theory_classes_enrolled,
+        isth.pce AS practical_classes_enrolled
+    FROM instructor_semester_teaching_hours AS isth
+    JOIN courses AS c ON isth.course_id = c.id
+    JOIN academic_levels AS al ON c.level_id = al.id
+    WHERE isth.vtu_id = ?
+    AND isth.academic_year_start = ?;
+`;
+
+
+async function getEmpDetailsByVtuId(vtu_id, year) {
+    let result;
+    let connection;
+    const payload = [vtu_id, year];
+
+    try {
+        connection = await connect();
+
+        const [data] = await connection.query(getISTHDetailsByVtuIdAndYear, payload);
+
+        if (data.length === 0) {
+            result = generateResponse(
+                true,
+                "Details for particular year for particlar employee not found",
+                400,
+                { vtu_id, year }
+            );
+        } else {
+            result = generateResponse(
+                false,
+                "ISTH Details Found, details sent!!",
+                200,
+                { details: data[0] }
+            );
+        }
+
+
+    } catch (error) {
+        console.log("ERROR    Something went wrong when fetching ISTH Details of particular Employee of particular year");
+        result = generateResponse(true, error.message, error.errno, payload);
+    } finally {
+        connection.end();
+        return result;
+    }
+}
+
+
+module.exports = getEmpDetailsByVtuId;
