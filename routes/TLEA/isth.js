f3ccdbe58c81e6f80645bcba5698a5d2b393b59f
shashiKiran <kiranshashi110@gmail.com>
2024-04-29 22:05:59 +0530

    Update Import Routes and create and complete route for ISTH Insertion

--- routes/TLEA/isth.js
+++ routes/TLEA/isth.js
@@ -3,12 +3,13 @@ const express = require("express");
 
 
 // Imports from server files
-const notAuthorizedResponse = require("../helpers/response/notAuthorizedResponse");
-const isAuthorized = require("../helpers/authorization/isAuthorised");
+const notAuthorizedResponse = require("./../../helpers/response/notAuthorizedResponse");
+const isAuthorized = require("./../../helpers/authorization/isAuthorised");
 
-const validateArguments = require("../helpers/validation/validateArguments");
-const invalidArguments = require("../helpers/validation/invalidArguments");
+const validateArguments = require("./../../helpers/validation/validateArguments");
 const handleGetDetailsByVtuIdAndYear = require("../../handlers/TLEA/ISTH/handleGetDetailsByVtuIdAndYear");
+const invalidArgumentsResponse = require("../../helpers/response/invalidArgumentsResponse");
+const handleInsertOneISTHRow = require("../../handlers/TLEA/ISTH/handleEnterISTHDetails");
 
 
 //Configuring the Backend middlewares and dependencies
@@ -40,14 +41,52 @@ router.route("/:vtu_id")
 
         // The employee can fetch his own details or the hod can access anyone's details
         if (vtu_id != userId && !isAuthorized(req, 4))
-            result = notAuthorizedResponse(req, res, arguments);
-        
-        else if (validateArguments(...Object.values(arguments)))
-            result = invalidArguments(req, res, arguments);
-        
-        else
-            result = handleGetDetailsByVtuIdAndYear(vtu_id, academic_year_start);
+            result = notAuthorizedResponse(req, res);
 
+        else if (validateArguments(...Object.values(arguments)))
+            result = invalidArgumentsResponse(req, res, arguments);
+
+        else
+            result = await handleGetDetailsByVtuIdAndYear(vtu_id, academic_year_start);
+
+
+        res.send(result);
+    })
+
+    // ENPOINT(/tlea/isth/:vtu_id)
+    // This Route will Add single Row to the isth Table
+    .post(async function (req, res) {
+        const { vtu_id } = req.params;
+        const { vtu_id: userId } = req.session;
+
+        // These are the required Fields to perform Transaction
+        const {
+            course_id = "not-entered",
+            semester = "not-entered",
+            tce = "not-entered",
+            pce = "not-entered",
+            startYear: academic_year_start = "not-entered",
+            endYear: academic_year_end = "not-entered"
+        } = req.body;
+
+        const arguments = {
+            vtu_id,
+            course_id,
+            semester,
+            tce,
+            pce,
+            academic_year_start,
+            academic_year_end
+        };
+
+        if ((vtu_id !== userId) && !isAuthorized(req, 4))
+            result = notAuthorizedResponse(req, res);
+
+        else if (!validateArguments(...Object.values(arguments)))
+            result = invalidArgumentsResponse(req, res, arguments);
+
+        else
+            result = await handleInsertOneISTHRow(arguments);
 
         res.send(result);
     })
