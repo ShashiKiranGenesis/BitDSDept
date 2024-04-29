b9c6712f6ffb837debb4db86e70376eb0b20f468
shashiKiran <kiranshashi110@gmail.com>
2024-04-29 21:52:55 +0530

    Update Some Import Files in TLEA.js

--- routes/TLEA/TLEA.js
+++ routes/TLEA/TLEA.js
@@ -3,13 +3,6 @@ const express = require("express");
 
 
 // Imports from server files
-const notAuthorizedResponse = require("../helpers/response/notAuthorizedResponse");
-const isAuthorized = require("../helpers/authorization/isAuthorised");
-const defaultResponse = require("../helpers/response/defaultResponse");
-
-const validateArguments = require("../helpers/validation/validateArguments");
-const invalidArguments = require("../helpers/validation/invalidArguments");
-
 const isthRoutes = require("./isth");
 
 //Configuring the Backend middlewares and dependencies
