f9b39ce7a34a3b2dfcf679dd796ad3227a3d97bf
shashiKiran <kiranshashi110@gmail.com>
2024-05-05 21:56:44 +0530

    Create MySql Query file for fetching all rows of the Employee Designations Table

---
+++ database/connector tables/employee_designation/getAllDesignations.js
@@ -0,0 +1,53 @@
+// Imports from Server Files
+const connect = require("./../../connect");
+const generateResponse = require("../../../helpers/response/generateResponse");
+
+
+//The Query to get all rows of the employee_designations Table
+const getAllDesignationsQuery = `
+    SELECT *
+    FROM employee_designation
+    ORDER BY vtu_id;
+`;
+
+
+async function getAllDesignations() {
+    let connection;
+    let result;
+
+    try {
+        connection = await connect();
+        const [data] = await connection.query(getAllDesignationsQuery);
+        
+        if (data.length === 0)
+            result = generateResponse(
+                true,
+                "No Employees with Designations Found",
+                200,
+                data
+            );
+        else
+            result = generateResponse(
+                false,
+                "All Employees with Designations details Sent",
+                200,
+                data
+            );
+    } catch (error) {
+        console.log("ERROR    Something went Wrong while Fetching all designations");
+
+        result = generateResponse(
+            true,
+            error.message,
+            error.errno,
+            {}
+        );
+
+    } finally {
+        connection.end();
+
+        return result;
+    }
+}
+
+module.exports = getAllDesignations;
