cbfcb4f522ffbe98a0433d1475ce805b953659fc
shashiKiran <kiranshashi110@gmail.com>
2024-05-05 22:20:20 +0530

    Create MySql Query file to get Designations by VtuId

---
+++ database/connector tables/employee_designation/getDesignationsByVtuId.js
@@ -0,0 +1,54 @@
+// Imports from Server Files
+const connect = require("../../connect");
+const generateResponse = require("../../../helpers/response/generateResponse");
+
+
+//The Query to get Designations of a particular employee
+const getDesignationByVtuIdQuery = `
+    SELECT *
+    FROM employee_designation
+    WHERE vtu_id = ?;
+`;
+
+
+async function getDesignationByVtuId(vtu_id) {
+    let arguments = { vtu_id };
+    let connection;
+    let result;
+
+    try {
+        connection = await connect();
+        const [data] = await connection.query(getDesignationByVtuIdQuery, [vtu_id]);
+
+        if (data.length === 0)
+            result = generateResponse(
+                true,
+                "Employee with entered vtu_id has not been assigned any post",
+                200,
+                arguments
+            );
+        else
+            result = generateResponse(
+                false,
+                "All Designations of Employee sent",
+                200,
+                data
+            );
+    } catch (error) {
+        console.log("ERROR    Something went Wrong while fetching designations of given vtu_id");
+
+        result = generateResponse(
+            true,
+            error.message,
+            error.errno,
+            {}
+        );
+
+    } finally {
+        connection.end();
+
+        return result;
+    }
+}
+
+module.exports = getDesignationByVtuId;
