3d9e90e7cd975c8cf36e42bb53738f18f27f07c2
shashiKiran <kiranshashi110@gmail.com>
2024-05-06 17:29:47 +0530

    Create Designation Routes with initially get route

---
+++ routes/designationRoutes.js
@@ -0,0 +1,40 @@
+//Imports from node modules
+const express = require("express");
+
+const validArguments = require("./../helpers/validation/validateArguments");
+const isAuthorized = require("../helpers/authorization/isAuthorised");
+
+const generateResponse = require("../helpers/response/generateResponse");
+const notAuthorizedResponse = require("./../helpers/response/notAuthorizedResponse");
+const invalidArgumentsResponse = require("../helpers/response/invalidArgumentsResponse");
+
+const getAllDesignations = require("../database/tables/designations/getAllDesignations");
+
+
+//Configuring the Backend middlewares and dependencies
+const router = express.Router({ strict: true });
+
+/////////////////////////////////////////////////////////////////////////
+///////////////////////////All the  Routes///////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+
+// ENDPOINT("/designations")
+// This route will fetch all the available posts in BIT
+router.route("/")
+    .get(async function (req, res) {
+        let result;
+
+        if (!isAuthorized(req, 2))
+            result = notAuthorizedResponse(req, res);
+        else
+            result = await getAllDesignations();
+
+        res.send(result);
+    })
+
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+module.exports = router;
