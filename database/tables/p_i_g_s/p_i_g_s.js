4c266185257476aa067fbc531cbcca52b71b8b15
shashiKiran <kiranshashi110@gmail.com>
2024-05-18 15:15:46 +0530

    Create Table file for p_i_g_s table

---
+++ database/tables/p_i_g_s/p_i_g_s.js
@@ -0,0 +1,54 @@
+// Imports from server files
+const connect = require("../../connect");
+
+
+// The original programmes_invited_as_guest_speaker table schema
+const createTableQuery = `
+    CREATE TABLE programmes_invited_as_guest_speaker (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        vtu_id VARCHAR(15) NOT NULL,
+        date_of_event DATE,
+        title_of_lecture VARCHAR(50) NOT NULL,
+        title_of_conference VARCHAR(50) NOT NULL,
+        event_level ENUM('I', 'N', 'S', 'C', 'R') NOT NULL,
+        organized_by VARCHAR(50) NOT NULL,
+        api_score TINYINT NOT NULL,
+
+        FOREIGN KEY (vtu_id) REFERENCES employees(vtu_id),
+
+        CONSTRAINT unique_row_pigs UNIQUE(
+            vtu_id,
+            date_of_event,
+            title_of_lecture,
+            title_of_conference,
+            organized_by, 
+            api_score
+        )
+    );
+`;
+// More details on The Table
+/*
+duration is in days
+*/
+
+// For any Altering in the future
+const alterTableQuery = `
+	
+`;
+
+(async function () {
+    let connection;
+    try {
+        connection = await connect();
+
+        /* Load your Query here */
+        await connection.query(createTableQuery);
+
+        console.log("PASS    CODE: DB_TC_25");
+    } catch (error) {
+        console.error("ERROR    CODE: DB_TC_25");
+        console.error(error.message);
+    } finally {
+        connection.end();
+    }
+})();
