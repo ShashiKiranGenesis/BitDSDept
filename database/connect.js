b4d0d61ec8bd1547fec0ee97618c9f5e80bc4730
shashiKiran <kiranshashi110@gmail.com>
2024-05-19 17:49:57 +0530

    Update Connect.js So as to handle Errors while connecting to DataBase

--- database/connect.js
+++ database/connect.js
@@ -1,22 +1,38 @@
-// All the imports must be in alphabetical order
-
+// Importing from Node Modules
 const dotenv = require("dotenv");
 const mysql = require("mysql2/promise");
 
-//Configuring the Backend middlewares and dependencies
+// Importing from Server Files
+const generateResponse = require("../helpers/response/generateResponse");
 
+// Configuring the Backend middlewares and dependencies
 dotenv.config();
 
 // Create the connection to database
-
 async function connect() {
-	const connection = await mysql.createConnection({
-		host: process.env.DB_HOST,
-		user: process.env.DB_USERNAME,
-		database: process.env.DB_DATABASE,
-		password: process.env.DB_PASSWORD
-	});
-	return connection;
+	try {
+		const connection = await mysql.createConnection({
+			host: process.env.DB_HOST,
+			user: process.env.DB_USERNAME,
+			database: process.env.DB_DATABASE,
+			password: process.env.DB_PASSWORD
+		});
+		return connection;
+
+	} catch (error) {
+		console.log("Something went wrong while connecting to Data Base");
+
+		const result = generateResponse(
+			true,
+			error.message,
+			error.errno,
+			error
+		);
+		console.log(result);
+
+		return result;
+	}
+
 }
 
 module.exports = connect;
