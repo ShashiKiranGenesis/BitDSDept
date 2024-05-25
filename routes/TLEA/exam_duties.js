c6e8a0457a365f0f9b29da7ce2d184a4d34a9ff4
shashiKiran <kiranshashi110@gmail.com>
2024-05-25 15:38:20 +0530

    Implement Router file for Examination duties table

--- routes/TLEA/exam_duties.js
+++ routes/TLEA/exam_duties.js
@@ -1,6 +1,20 @@
 // Imports from node modules
 const express = require("express");
 
+// Importing MySQL transaction files for examination_duties table operations
+const getExaminationDutiesByVtuIdAndAcademicYear = require("./../../database/tables/exam_duties/getEDByVTUidAndYear");
+const deleteExaminationDutyById = require("./../../database/tables/exam_duties/deleteEDById");
+const insertSingleExaminationDuty = require("./../../database/tables/exam_duties/insertSingleED");
+const insertMultipleExaminationDuties = require("./../../database/tables/exam_duties/insertMultipleED");
+const getExaminationDutyById = require("./../../database/tables/exam_duties/getEDById");
+
+// Import from Server Files
+const validateArguments = require("../../helpers/validation/validateArguments");
+const invalidArgumentsResponse = require("../../helpers/response/invalidArgumentsResponse");
+const isAuthorized = require("../../helpers/authorization/isAuthorised");
+const notAuthorizedResponse = require("../../helpers/response/notAuthorizedResponse");
+const generateResponse = require("../../helpers/response/generateResponse");
+
 // Configuring the Backend middlewares and dependencies
 const router = express.Router({ strict: true });
 
@@ -8,7 +22,115 @@ const router = express.Router({ strict: true });
 /////////////////////////// All the Routes /////////////////////////////
 /////////////////////////////////////////////////////////////////////////
 
-// Add your routes here
+router.route("/:vtu_id")
+
+    // GET /tlea/examination_duties/:vtu_id
+    .get(async function (req, res) {
+        const { vtu_id = "not-entered" } = req.params;
+        const {
+            academic_year_start = "not-entered",
+            academic_year_end = "not-entered",
+            vtu_id: userId = "not-entered"
+        } = req.session;
+
+        let result, arguments;
+
+        arguments = { vtu_id, academic_year_start, academic_year_end };
+
+        if (vtu_id !== userId && !isAuthorized(req, 5))
+            result = notAuthorizedResponse(req, res);
+        else if (!validateArguments(...Object.values(arguments)))
+            result = invalidArgumentsResponse(req, res, arguments);
+        else
+            result = await getExaminationDutiesByVtuIdAndAcademicYear(arguments);
+
+        res.send(result);
+    })
+
+    // POST /tlea/examination_duties/:vtu_id
+    .post(async function (req, res) {
+        const { vtu_id = "not-entered" } = req.params;
+        const {
+            exam_type = "not-entered",
+            duty_assigned = "not-entered",
+            extent_carried_out = "not-entered",
+            api_score = "not-entered"
+        } = req.body;
+        const {
+            academic_year_start = "not-entered",
+            academic_year_end = "not-entered",
+            vtu_id: userId = "not-entered"
+        } = req.session;
+
+        let result, arguments;
+
+        arguments = { vtu_id, exam_type, duty_assigned, extent_carried_out, api_score, academic_year_start, academic_year_end };
+
+        if (vtu_id !== userId && !isAuthorized(req, 5))
+            result = notAuthorizedResponse(req, res);
+        else if (!validateArguments(...Object.values(arguments)))
+            result = invalidArgumentsResponse(req, res, arguments);
+        else
+            result = await insertSingleExaminationDuty(arguments);
+
+        res.send(result);
+    })
+
+    // DELETE /tlea/examination_duties/:id
+    .delete(async function (req, res) {
+        const { vtu_id: id } = req.params;
+        const { vtu_id: userId } = req.session;
+
+        let result;
+
+        // If User is Admin Delete Right Away!
+        if (isAuthorized(req, 5))
+            result = await deleteExaminationDutyById({ id });
+
+        // If not check if user is deleting his own row
+        else {
+            const rowData = await getExaminationDutyById({ id });
+            if (rowData?.error)
+                result = generateResponse(
+                    true,
+                    "Could not delete This record for some reason, try again later",
+                    400,
+                    { id }
+                );
+            else if (rowData?.payload?.vtu_id === userId)
+                result = await deleteExaminationDutyById({ id });
+            else
+                result = notAuthorizedResponse(req, res);
+        }
+
+        res.send(result);
+    });
+
+// POST /tlea/examination_duties/m/:vtu_id
+router.post("/m/:vtu_id", async function (req, res) {
+    const rows = req.body;
+    const { vtu_id = "not-entered" } = req.params;
+    const {
+        academic_year_start = "not-entered",
+        academic_year_end = "not-entered",
+        vtu_id: userId = "not-entered"
+    } = req.session;
+
+    let result, arguments;
+
+    arguments = { vtu_id, academic_year_start, academic_year_end };
+
+    if (vtu_id !== userId && !isAuthorized(req, 5))
+        result = notAuthorizedResponse(req, res);
+    else if (!validateArguments(...Object.values(arguments)))
+        result = invalidArgumentsResponse(req, res, arguments);
+    else
+        result = await insertMultipleExaminationDuties(
+            rows.map(row => ({ ...row, ...arguments }))
+        );
+
+    res.send(result);
+});
 
 /////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////
